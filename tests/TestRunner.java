222d0901de9de01fe12a53d5a0a529d2f775c84e
Rulox <marreror91@gmail.com>
2015-05-30 14:42:08 +0100

    Added TestRunner for all tests

---
+++ tests/TestRunner.java
@@ -0,0 +1,8 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@SuiteClasses({MundoTest.class, SubmarinoHundidoTest.class, TransatlanticoHundidoTest.class, YateHundidoTest.class})
+public class TestRunner {}
